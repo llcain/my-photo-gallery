1370a1089c5a8a4ccc623f14825a3c0abd638f4c
llcain <llcain2012@gmail.com>
2024-05-06 23:23:06 -0500

    Added all photos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 
 import './App.scss';
+import photo21 from './images/photo21.jpg';
+import photo22 from './images/photo22.jpg';
+import photo25 from './images/photo25.jpg';
+import photo23 from './images/photo23.jpg';
+import photo1 from './images/photo1.jpg';
+import photo16 from './images/photo16.jpg';
+import photo2 from './images/photo2.jpg';
+import photo3 from './images/photo3.jpg';
+import photo4 from './images/photo4.jpg';
+import photo5 from './images/photo5.jpg';
+import photo6 from './images/photo6.jpg';
+import photo7 from './images/photo7.jpg';
+import photo8 from './images/photo8.jpg';
+import photo9 from './images/photo9.jpg';
+import photo10 from './images/photo10.jpg';
+import photo11 from './images/photo11.jpg';
+import photo12 from './images/photo12.jpg';
+import photo13 from './images/photo13.jpg';
+import photo14 from './images/photo14.jpg';
+import photo15 from './images/photo15.jpg';
+import photo17 from './images/photo17.jpg';
+import photo18 from './images/photo18.jpg';
+import photo19 from './images/photo19.jpg';
+import photo20 from './images/photo20.jpg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome</h1>
-      </header>
-    </div>
+    <>
+      <h1>My Photo Image Gallery</h1>
+      <div className='container'>
+        <div className='img-grid'>
+          <img src={photo21} alt='styling'/>
+          <img src={photo22} alt='laughing'/>
+          <img src={photo25} alt='back'/>
+          <img src={photo23} alt='set'/>
+          <img src={photo1} alt='top'/>
+          <img src={photo16} alt='sit'/>
+          <img src={photo2} alt='chain'/>
+          <img src={photo3} alt='me laughing'/>
+          <img src={photo4} alt='me laugh'/>
+          <img src={photo5} alt='me playing'/>
+          <img src={photo6} alt='me visit'/>
+          <img src={photo7} alt='myself'/>
+          <img src={photo8} alt='shape'/>
+          <img src={photo9} alt='my family'/>
+          <img src={photo10} alt='my name'/>
+          <img src={photo11} alt='change'/>
+          <img src={photo12} alt='feet'/>
+          <img src={photo13} alt='sun'/>
+          <img src={photo14} alt='cloud'/>
+          <img src={photo15} alt='day'/>
+          <img src={photo17} alt='time'/>
+          <img src={photo18} alt='watch'/>
+          <img src={photo19} alt='teeth'/>
+          <img src={photo20} alt='pillow'/>
+            
+
+          
+        </div>
+      </div>
+    </>
   );
 }
 
